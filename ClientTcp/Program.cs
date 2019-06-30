1513f48723c6187c47be45bb504e002e6c1eeb60
igor <isakov.dev921@gmail.com>
2019-06-30 16:44:13 +0300

    создал клиента

---
+++ ClientTcp/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientTcp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string ip = "127.0.0.1";
+            const int port = 8888;
+           
+            var tcpEEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+            
+            var tcpSocket = new Socket(AddressFamily.InterNetwork,
+                SocketType.Stream, ProtocolType.Tcp);
+
+            Console.WriteLine("Введите сообщение");
+            var message = Console.ReadLine();
+
+            //кодируем смс
+            var data = Encoding.UTF8.GetBytes(message);
+
+            //подключаемся к сокету как клиент
+            tcpSocket.Connect(tcpEEndPoint);
+
+            //отплавляем смс
+            tcpSocket.Send(data);
+            
+            var buffer = new byte[256];
+            var size = 0;
+
+            //ответ от сервера
+            var answer = new StringBuilder();
+
+            do
+            {               
+                size = tcpSocket.Receive(buffer);
+                answer.Append(Encoding.UTF8.GetString(buffer, 0, size));
+            }
+            while (tcpSocket.Available > 0);
+
+            Console.WriteLine(answer);
+            tcpSocket.Shutdown(SocketShutdown.Both);
+            tcpSocket.Close();
+
+            Console.ReadLine();
+        }
+    }
+}
--- ServetTestTCP/Program.cs
+++ ServetTestTCP/Program.cs
@@ -16,59 +16,69 @@ namespace ServetTestTCP
             const int port = 8888;
 
             //точка подключения
-            var tcpEEndPoint = new IPEndPoint(IPAddress.Parse(ip), port); 
+            var tcpEEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
 
             //создания сокета для тсп протокола
-            var tcpSocket = new Socket(AddressFamily.InterNetwork, 
-                SocketType.Stream, ProtocolType.Tcp); 
+            var tcpSocket = new Socket(AddressFamily.InterNetwork,
+                SocketType.Stream, ProtocolType.Tcp);
 
-            //перевести сокет в режим ожидание, 
-            //этому сокету нужно слушать конкретный  порт
-            tcpSocket.Bind(tcpEEndPoint);
-
-            //очередь подключения
-            tcpSocket.Listen(5); 
-
-            //процесс посслушивания
-            while (true)
+            try
             {
-                //обработчик на прием сообщений
-                var listener = tcpSocket.Accept();
 
-                //буфер куда будем принимать данные массива
-                //хранилище данных (максимум)
-                var buffer = new byte[256];
+                //перевести сокет в режим ожидание, 
+                //этому сокету нужно слушать конкретный  порт
+                tcpSocket.Bind(tcpEEndPoint);
 
-                //переменная реально полученных байт
-                var size = 0;
+                //очередь подключения
+                tcpSocket.Listen(10);
+                Console.WriteLine("Сервер запущен. Ожидание подключений...");
 
-                //собираем полученные данные при помощи билдера
-                var data = new StringBuilder();
-
-
-                //цикл с пост условием
-                do
+                //процесс послушивания
+                while (true)
                 {
-                    //получаем реальное колиество байт
-                    size = listener.Receive(buffer);
+                    //обработчик на прием сообщений
+                    var listener = tcpSocket.Accept();
 
-                    //собираем все сообщения из кусочков, после кодирования
-                    data.Append(Encoding.UTF8.GetString(buffer, 0, size));
+                    //буфер куда будем принимать данные массива
+                    //хранилище данных (максимум)
+                    var buffer = new byte[256];
+
+                    //переменная реально полученных байт
+                    var size = 0;
+
+                    //собираем полученные данные при помощи билдера
+                    var data = new StringBuilder();
+
+
+                    //цикл с пост условием
+                    do
+                    {
+                        //получаем реальное колиество байт
+                        size = listener.Receive(buffer);
+
+                        //собираем все сообщения из кусочков, после кодирования
+                        data.Append(Encoding.UTF8.GetString(buffer, 0, size));
+
+                    }
+                    while (listener.Available > 0);
+
+                    Console.WriteLine(DateTime.Now.ToShortTimeString() + ": " + data.ToString());
+
+                    //отправляем ответ (закодируем и отплавляем листенеру)
+                    listener.Send(Encoding.UTF8.GetBytes("Успех"));
+
+                    //выключаем подключение 
+                    listener.Shutdown(SocketShutdown.Both);
+
+                    //Закрываем
+                    listener.Close();
 
                 }
-                while (listener.Available > 0);
 
-                Console.WriteLine(data); //TODO: проверить .ToString
-
-                //даем обратный ответ (закодируем и отплавляем листенеру)
-                listener.Send(Encoding.UTF8.GetBytes("Успех"));
-
-                //выключаем подключение 
-                listener.Shutdown(SocketShutdown.Both);
-
-                //Закрываем
-                listener.Close();
-
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
             }
 
 
